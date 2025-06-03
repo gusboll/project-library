dce02aa32c6505ef20def712b82394294fd7313f
gusboll <augusto.sboll@gmail.com>
2025-06-03 13:53:09 -0300

    finish the project
    added the codes for the js and finished the styling

--- script.js
+++ script.js
@@ -1,52 +1,116 @@
+const addBookBtn = document.getElementById("addBookBtn");
+const dialog = document.getElementById("bookDialog");
+const cancelButton = document.getElementById("cancel");
+const submitButton = document.getElementById("dialog-confirm-button");
+const formDialog = document.getElementById("formDialog");
+const cardsDisplay = document.querySelector(".cards");
+const readButton = document.querySelector(".card-read");
+
+
+
 
 // array for stored books, inserted by the user
 const myLibrary = [];
 
+
+// addbook button and cancelbutton on the addbook dialog
+addBookBtn.addEventListener("click", function(){
+    formDialog.reset();
+    dialog.showModal();
+}) 
+
+cancelButton.addEventListener("click", function(){
+    dialog.close();
+})
+
 // constructor for books
-function Book = (title, author, pageNumber, read){
+function Book (title, author, pageNumber, read, bookId){
     this.title = title;
     this.author = author;
     this.pageNumber = pageNumber;
     this.read = read;
-    // check again the last class to remember how to finish this construct
+    this.bookId = bookId;
 };
 
-//function that gets the new books infos and stores them in the array with an unique id
-function addBookToLibrary(){
-// function that takes some arguments
-// function that creates a book from those arguments
-// stores the book in the array
-// all new books should have an unique id (data-attribute) ("crypto.randomUUID()")
-
+Book.prototype.toggleRead = function() {
+    this.read = this.read === "yes" ? "no" : "yes";
 };
+
 
 // function that loops through the myLibrary array
 // displays that book each one in a card
 function displayLibrary(){
-    
-};
+    cardsDisplay.innerHTML = "";
+// array.foreach to show the infos inserted by the user in the cards
+    myLibrary.forEach(function(book){
+        let newCard = document.createElement("div");
+        newCard.classList.add("card");
 
-// event listener for a button that brings up a form to input all the infos for the new book
-// with that, add it to the library array and to a new card display
-addBookBtn.addEventListener("click", (){
+        newCard.innerHTML = `
+        <div class = "card-author">Author: ${book.author}</div>
+        <div class = "card-title">Title: ${book.title}</div>
+        <div class = "card-pages">Number of Pages: ${book.pageNumber}</div>
+        <button class = "card-read">${book.read === "yes" ? "Read" : "Unread"}</button>
+        <button class = "card-remove">Remove</button>`;
 
+        const readButton = newCard.querySelector(".card-read");
+        readButton.classList.remove("read-yes", "read-no");
+        readButton.classList.add(book.read === "yes" ? "read-yes" : "read-no")
+
+        readButton.addEventListener("click", () =>{
+            book.toggleRead();
+            displayLibrary();
+        })
+
+        const removeBtn = newCard.querySelector(".card-remove");
+        removeBtn.addEventListener("click", () =>{
+            const index = myLibrary.findIndex(b => b.bookId === book.bookId);
+            if (index > - 1){
+                myLibrary.splice(index, 1);
+                displayLibrary();
+            }
+        })
+
+        cardsDisplay.appendChild(newCard);
+    })
+}
+
+
+
+submitButton.addEventListener("click", function(event){
+    event.preventDefault();
+    dialog.close();
+    // retrieving the forms data
+    let bookAuthor = document.getElementById("author").value;
+    let bookName = document.getElementById("bookName").value;
+    let bookPages = document.getElementById("pages").value;
+    let bookRead = document.getElementById("read").value;
+    let bookId = crypto.randomUUID();
+    const newBook = new Book (bookName, bookAuthor, bookPages, bookRead, bookId);
+    myLibrary.push(newBook);
+    displayLibrary();
+    return bookAuthor, bookName, bookPages, bookRead, bookId;
 })
 
-// if the submit button gives an issue by not do what i expect it to do,
-// should consider using the "event.preventDefault();" 
-// https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+
+
+
+
+
+
+
 
 // event listener for button that removes that unique id from the card of books
-removeBookBtn.addEventListener("click", (){
-
-})
+//removeBookBtn.addEventListener("click", (){
+//
+//})
 
 // event listener for button that marks that book as read/unread in the card of books
 // create Book prototype function that toggle a book instance `read` status
-readBtn.addEventListener("click", (){
+//readBtn.addEventListener("click", (){
+//
+//})
 
-})
-
-unreadBtn.addEventListener("click", (){
-
-})
+//unreadBtn.addEventListener("click", (){
+//
+//})
